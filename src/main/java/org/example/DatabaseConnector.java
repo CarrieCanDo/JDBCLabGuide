cf2120abe9b5157852f9139642c11bb6b2322dc4
CarrieCanDo <carrie.a.wright@outlook.com>
2024-08-28 16:32:45 -0400

    Committing assignment

---
+++ src/main/java/org/example/DatabaseConnector.java
@@ -0,0 +1,53 @@
+package org.example;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DatabaseConnector {
+
+    // Connect to SQLite database
+    public Connection connect() {
+        Connection connection = null;
+        try {
+            // SQLite connection string
+            String url = "jdbc:sqlite:C:/Users/User/Carrie/sqlite/BookStore";
+            connection = DriverManager.getConnection(url);
+            System.out.println("Successfully connected to the database!");
+        } catch (SQLException e) {
+            System.out.println("Error connecting to the database.");
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    public void selectAllBooks() {
+        Connection connection = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+        try {
+            connection = this.connect();
+            String sql = "SELECT * FROM books";
+            stmt = connection.createStatement();
+            rs = stmt.executeQuery(sql);
+
+            while (rs.next()) {
+                System.out.println(rs.getString("title") + "\t" +
+                        rs.getString("author") + "\t" +
+                        rs.getDouble("price"));
+            }
+        } catch (SQLException e) {
+            System.out.println("Error executing SELECT statement");
+            e.printStackTrace();
+        } finally {
+            try {
+                if (rs != null) rs.close();
+                if (stmt != null) stmt.close();
+                if (connection != null) connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,9 @@
 package org.example;
 
 public class Main {
+    // Main method for testing
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        DatabaseConnector connector = new DatabaseConnector();
+        connector.selectAllBooks();
     }
 }
